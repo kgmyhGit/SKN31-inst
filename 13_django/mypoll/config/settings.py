952e032a44a75e91096daff0b2416b133dfcd391
Kim Sunghwan <kgmyh@naver.com>
2026-07-28 15:08:09 +0900

    up

--- 13_django/mypoll/config/settings.py
+++ 13_django/mypoll/config/settings.py
@@ -56,7 +56,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
-        'APP_DIRS': True,
+        'APP_DIRS': True, # app/templates
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.request',
